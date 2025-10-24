fcb95f7b0b252268278f222abfbd8ed131d80835
Tsuten <hiimhereforyouguys@gmail.com>
2025-10-24 11:28:20 +0900

    feat: 通知機能のとりあえずの外枠を実装

--- server/CalorieMeter/CalorieMeter/settings.py
+++ server/CalorieMeter/CalorieMeter/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'users',
     'core',
     'calendars',
+    'notifications',
 ]
 
 MIDDLEWARE = [
--- server/CalorieMeter/components/templatetags/header.py
+++ server/CalorieMeter/components/templatetags/header.py
@@ -1,5 +1,6 @@
 from django import template
-
+from notifications.models import Notification
+from users.models import CustomUser
 register = template.Library()
 
 @register.inclusion_tag('header.html', takes_context=True)
@@ -10,7 +11,7 @@ def header(context):
          "href": "/"},
         {"label": "Calendar",
          "icon": "calendar",
-         "href": "/calendar"},
+         "href": "/calendars/m/2025/10"},
         {"label": "Statistics",
          "icon": "bar-chart-3",
          "href": "/statistics"},
@@ -19,4 +20,6 @@ def header(context):
          "href": "/history"},
     ]
 
-    return {'header_contents': header_contents}
+    notifications = Notification.objects.filter(to_user=CustomUser.objects.get(id="f68eea44-9a37-4b44-a619-11b2c7135665")).order_by('-created_at')
+
+    return {'header_contents': header_contents, 'notifications': notifications}
