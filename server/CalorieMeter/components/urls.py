9a9e60947f2891369a780d8658bebf2b3750abbc
Tsuten <hiimhereforyouguys@gmail.com>
2025-10-28 15:05:29 +0900

    feat: アップロードviewの雛形の作成

--- server/CalorieMeter/components/urls.py
+++ server/CalorieMeter/components/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, upload, calendar, statistics
+from .views import index, UploadView, calendar, statistics, profile
 
 urlpatterns = [
     path('', index, name='index'),
-    path('upload', upload, name='upload'),
+    path('upload', UploadView.as_view(), name='upload'),
     path('calendar', calendar, name='calendar'),
     path('statistics', statistics, name='statistics'),
+    path('profile', profile, name='profile'),
 ]
--- server/CalorieMeter/components/views.py
+++ server/CalorieMeter/components/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views import View
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
-def upload(request):
-    return render(request, 'upload.html')
+class UploadView(View):
+    def get(self, request):
+        return render(request, 'upload.html')
+    def post(self, request):
+        print('POSTデータ:', request.POST)
+        if not request.FILES:
+            return render(request, 'upload.html', {'error': 'ファイルがありません'})
+        for _, file in request.FILES.items():
+            print(f'ファイル名: {file.name}, コンンテンツタイプ: {file.content_type}')
+        return render(request, 'upload.html')
 
 def calendar(request):
     return render(request, 'calendar.html')
@@ -13,3 +22,9 @@ def calendar(request):
 # @login_required
 def statistics(request):
     return render(request, 'statistics.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def record(request):
+    return render(request, 'record.html')
